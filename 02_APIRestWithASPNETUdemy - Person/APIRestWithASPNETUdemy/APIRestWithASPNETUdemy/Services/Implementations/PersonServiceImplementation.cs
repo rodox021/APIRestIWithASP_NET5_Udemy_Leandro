20668e52ac7f982258ddac5fac15866f31ea36d1
Rodolfo Leal Braga <75742317+rodox021@users.noreply.github.com>
2022-07-07 21:58:43 -0300

    implementando os metodos. update, delete, findById

--- 02_APIRestWithASPNETUdemy - Person/APIRestWithASPNETUdemy/APIRestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
+++ 02_APIRestWithASPNETUdemy - Person/APIRestWithASPNETUdemy/APIRestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -17,40 +17,81 @@ namespace APIRestWithASPNETUdemy.Services.Implementations
             _context = context;
         }
 
-        public Person Create(Person person)
-        {
-            return person;
-        }
-
-        public void Delete(long Id)
-        {
-
-        }
-
+        //----------------------------------------------------------------------------------
         public List<Person> FindAll()
         {
-          
-
             return _context.Persons.ToList();
         }
 
-
+        //----------------------------------------------------------------------------------
         public Person FindById(long id)
         {
-            return new Person
-            {
-                Id = 1,
-                FirstName = "Rodolfo",
-                LastName = "Braga",
-                Address = "Maria da Graça",
-                Gender = "Male"
-            };
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
         }
+        //----------------------------------------------------------------------------------
 
-        public Person Update(Person person)
+        public Person Create(Person person)
         {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
             return person;
         }
-      
+        //----------------------------------------------------------------------------------
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id)) return new Person();
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+            return person;
+        }
+        //----------------------------------------------------------------------------------
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+
+        }
+
+        //----------------------------------------------------------------------------------
+
+        private bool Exists(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
+        }
     }
 }
