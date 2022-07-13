101fe1a5df0861525f848dba1ba614a9701fd240
Rodolfo Leal Braga <75742317+rodox021@users.noreply.github.com>
2022-07-13 20:56:38 -0300

    configurando a migrations no startup

---
+++ 07_APIRestWithASPNETUdemy -Migrations/APIRestWithASPNETUdemy/APIRestWithASPNETUdemy/Business/IPersonBusiness.cs
@@ -0,0 +1,14 @@
+﻿using APIRestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+
+namespace APIRestWithASPNETUdemy.Business
+{
+    public interface IPersonBusiness
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long Id);
+    }
+}
