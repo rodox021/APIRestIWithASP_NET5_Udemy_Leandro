848ee15b673ccfad1443c421d599ffc1dfd52c93
Rodolfo Leal Braga <75742317+rodox021@users.noreply.github.com>
2022-07-13 08:12:09 -0300

    iniciando a refatoração para arquitetura em camdas, Business e Repository ciradas

---
+++ 06_APIRestWithASPNETUdemy - Adotando camadas/APIRestWithASPNETUdemy/APIRestWithASPNETUdemy/Repository/IPersonRepository.cs
@@ -0,0 +1,15 @@
+﻿using APIRestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+
+namespace APIRestWithASPNETUdemy.Repository
+{
+    public interface IPersonRepository
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long Id);
+        bool Exists(long id);
+    }
+}
